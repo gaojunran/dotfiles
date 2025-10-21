08a7d76e96ea38861ab587a0d11be7a00e689bbd
junrangao <junrangao@tencent.com>
2025-10-21 14:47:15 +0800

    Update: env

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,12 @@
--- local wezterm = require 'wezterm'
+local wezterm = require 'wezterm'
 local platform = require('utils')
+local act = wezterm.action
 local config = {}
 
 
--- if platform.is_win then 
 config.default_prog = { 'nu' }
--- end
+if platform.is_win then 
+config.keys = {{ key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },}
+end
 
 return config
