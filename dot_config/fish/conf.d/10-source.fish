316baff760c866864e2719b5818cf29b3cd531b1
gaojunran <nebula2021@126.com>
2026-08-01 01:43:07 +0800

    Update

--- dot_config/fish/conf.d/10-source.fish
+++ dot_config/fish/conf.d/10-source.fish
@@ -15,7 +15,7 @@ fish_add_path -U $HOME/.moon/bin
 fish_add_path -U $PNPM_HOME
 fish_add_path -U $PNPM_HOME/bin
 fish_add_path -U -m $HOME/.local/bin
-
+fish_add_path -U -m $HOME/.local/bin/scripts
 
 # Linuxbrew. FIXME: 可能需要兼容性更好的方式
 if test -x /home/linuxbrew/.linuxbrew/bin/brew
---
+++ dot_local/bin/scripts/update-opencode-family.sh
@@ -0,0 +1,12 @@
+# omo slim
+bunx oh-my-opencode-slim@latest install
+
+# opencode
+mise use -g opencode
+
+# magic-context 看起来不需要更新
+# ccc 看起来没用
+
+# openchamber
+curl -fsSL https://raw.githubusercontent.com/openchamber/openchamber/main/scripts/install.sh | bash
+openchamber restart
